fcc302449f2d2fa07a32e3b12cfdb4ccb818e91a
Tavo <lopezgarciatavo02@gmail.com>
2024-08-31 13:43:55 -0600

    agregando funcionalidades y dependencias de entorno de trabajo, dev, sass-gulp y watch

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+//importar todo sobre la dependencia sass
+import * as dartSass from 'sass'
+//importa la funcion desde la dependencia gulp-sass
+import gulpSass from 'gulp-sass'
+//funcion src para acceder a la unicacion de los archivos
+//funcion dest para almacenar los archivos procesados
+//funcion watch para seguir escuchando cambios en el archivo
+import {src, dest, watch} from 'gulp'
+
+
+const sass =  gulpSass(dartSass)
+
+export function css (done){
+    src('src/scss/app.scss')
+        .pipe(sass())//para dar control de la ejecucion de las funcion
+        .pipe(dest('build/css'))
+    done ()
+}
+
+//funcion watch para que la ejecucion se mantenga
+
+export const dev = () =>{
+    watch('src/scss/app.scss', css)
+}
