423fe1c81db20a26f92324b65d8789ef3c06c0d4
Tavo <lopezgarciatavo02@gmail.com>
2024-09-02 20:27:49 -0600

    diseño del apartado de galeria, es responsive)

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-alert('desde app.js')
+document.addEventListener('DOMContentLoaded', function(){
+    crearGallery()
+})
+
+const crearGallery = () =>{
+
+        const gallery = document.querySelector('.gallery-imagenes')
+        const Cantidad_img = 16
+
+        for (let i = 1; i <= Cantidad_img; i++) {
+            const imagen = document.createElement('IMG')
+            imagen.src = `../src/img/Gallery/full/${i}.jpg`;
+            imagen.alt = 'Imagen Galeria';
+
+            gallery.appendChild(imagen)
+
+        }
+}
